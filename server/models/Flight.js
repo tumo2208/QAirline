73c214e078b6269ec782ff9e0cbff2157c18382a
harukatou <bqtrung1001@gmail.com>
2024-11-05 10:08:08 +0700

    Initial design for models.

---
+++ server/models/Flight.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const seatFlightSchema = new Schema({
+    class_type: { type: String, enum: ['Economy', 'Business'], required: true },
+    seat_count: { type: Number, required: true },
+});
+
+const flightSchema = new Schema({
+    flight_number: { type: String, required: true },
+    aircraft_id: { type: Schema.Types.ObjectId, ref: 'Aircraft', required: true },
+    departure_airport_id: { type: Schema.Types.ObjectId, ref: 'Airport', required: true },
+    arrival_airport_id: { type: Schema.Types.ObjectId, ref: 'Airport', required: true },
+    departure_time: { type: Date, required: true },
+    arrival_time: { type: Date, required: true },
+    price: { type: Number, required: true },
+    available_seats: [seatFlightSchema],
+    status: { type: String, enum: ['Scheduled', 'Delayed', 'Cancelled'], required: true },
+});
+
+// Temporary
+flightSchema.pre('save', async function (next) {
+    if (!this.isNew) return next();
+
+    const aircraft = await mongoose.model('Aircraft').findById(this.aircraft_id);
+    if (aircraft) {
+        this.seat_classes = aircraft.seat_classes.map(seatClass => ({
+            class_type: seatClass.class_type,
+            available_seats: seatClass.seat_count,
+        }));
+    } else {
+        return next(new Error('Aircraft not found for the flight.'));
+    }
+
+    next();
+});
+
+module.exports = mongoose.model('Flight', flightSchema);
