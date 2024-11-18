393baf450688a5080c0716bf60d5d90f0ed076c5
harukatou <bqtrung1001@gmail.com>
2024-11-18 10:48:46 +0700

    update profile method

--- server/routes/AuthRoute.js
+++ server/routes/AuthRoute.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { login, profile, register, updateIdentification } = require('../controller/AuthController');
+const { login, profile, register, update } = require('../controller/AuthController');
 const userVerification = require('../middleware/authMiddleware');
 
 // Register
@@ -13,6 +13,17 @@ router.post('/login', login);
 router.get('/profile', userVerification, profile);
 
 // Update Identification
-router.put('/profile/identification', userVerification, updateIdentification);
+router.put('/profile', userVerification, update);
+
+// Test Route
+router.get('/test', userVerification, async (req, res) => {
+    try {
+        const userId = req.user.id; // Decoded ID from middleware
+        res.json({ message: "Test route works", userId });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Error testing the route" });
+    }
+});
 
 module.exports = router;
