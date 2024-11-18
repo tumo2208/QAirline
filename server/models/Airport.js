74b46bb1780277a07b4708f1c4cf36873ade107c
harukatou <bqtrung1001@gmail.com>
2024-11-18 09:00:06 +0700

    Airport: airport_code, international

--- server/models/Airport.js
+++ server/models/Airport.js
@@ -5,7 +5,8 @@ const airportSchema = new Schema({
     name: { type: String, required: true },
     city: { type: String, required: true },
     country: { type: String, required: true },
-    continent: { type: String, required: true }
+    airport_code: { type: String, required: true, unique: true },
+    isInternational: { type: Boolean, default: false },
 });
 
 module.exports = mongoose.model('Airport', airportSchema);
