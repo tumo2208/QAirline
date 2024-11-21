867e7a75310acb413087fdcfe30a0a3a73105ee3
nguyentankdb17 <nguyentankdb17@gmail.com>
2024-11-21 09:06:03 +0700

    Update model

--- server/models/Booking.js
+++ server/models/Booking.js
@@ -4,10 +4,14 @@ const { Schema } = mongoose;
 const Ticket = require('./Ticket');
 
 const bookingSchema = new Schema({
-    booking_id: { type: String, required: true },
-    book_user_id: { type: String, ref: 'User', required: true },
-    tickets: [Ticket.schema],
+    flight_id: { type: String, ref: 'Flight', required: true },
+    passenger_id: { type: String, ref: 'User', required: true },
     booking_date: { type: Date, default: Date.now },
+    num_adult: { type: Number, required: true },
+    num_child: { type: Number, required: true },
+    num_infant: { type: Number, required: true },
+    tickets: [Ticket.schema],
+    total_price: { type: Number, required: true },
 });
 
 module.exports = mongoose.model('Booking', bookingSchema);
