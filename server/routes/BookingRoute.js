cd98dd84ac33d20d04e24ec13ad665b6167c7e7b
tumo2208 <tuadc1234@gmail.com>
2024-11-30 18:10:50 +0700

    User authorization and add function cancelTicket

--- server/routes/BookingRoute.js
+++ server/routes/BookingRoute.js
@@ -1,11 +1,13 @@
 const express = require('express');
-const {getMyBookings, makeBooking, cancelBooking} = require('../controller/BookingController');
+const {getMyBookings, getBookingByID, makeBooking, cancelBooking, cancelTicket} = require('../controller/BookingController');
 const userVerification = require('../middleware/authMiddleware');
 
 const router = express.Router();
 
 router.get('/myBookings', userVerification, getMyBookings);
+router.post('/getBookingByID', userVerification, getBookingByID);
 router.post('/newBooking', userVerification, makeBooking);
 router.post('/cancelBooking', userVerification, cancelBooking);
+router.post('/cancelTicket', userVerification, cancelTicket);
 
 module.exports = router;
