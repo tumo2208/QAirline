edd251baed77ff37466a1f8e0bdcb63050645fe8
tumo2208 <tuadc1234@gmail.com>
2024-11-30 17:16:09 +0700

    merge function makeBooking and makeTicket

--- server/models/Booking.js
+++ server/models/Booking.js
@@ -6,6 +6,7 @@ const Ticket = require('./Ticket');
 const bookingSchema = new Schema({
     flight_id: { type: String, ref: 'Flight', required: true },
     passenger_id: { type: Schema.Types.ObjectId, ref: 'User'},
+    class_type: { type: String, enum: ['Economy', 'Business']},
     booking_date: { type: Date, default: Date.now },
     num_adult: { type: Number, required: true },
     num_child: { type: Number, required: true },
