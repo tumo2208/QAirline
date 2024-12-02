44a3a141463d0b833fb4ae5436e6f8ad9808b2ae
nguyentankdb17 <nguyentankdb17@gmail.com>
2024-12-02 22:18:48 +0700

    Update mybooking UI

--- client/src/components/MyBooking/HomePage.js
+++ client/src/components/MyBooking/HomePage.js
@@ -1,5 +1,46 @@
-function HomePage(){
+import React from "react";
 
+function HomePage(){
+    return (
+        <div className="py-10" style={{backgroundImage: "url('https://wallpapercat.com/w/full/3/b/d/21204-1920x1200-desktop-hd-clouds-background-photo.jpg')"}}>
+            <div className="  border-4 mx-60 my-5 rounded-2xl shadow-lg"
+                 >
+                <div className="flex select-none cursor-pointer  justify-between text-white text-center w-full font-bold">
+                    <div className="bg-blue-800 text-lg rounded-tl-lg w-1/2 py-3">
+                        TRA CỨU MÃ ĐẶT CHỖ
+                    </div>
+                    <div className="bg-blue-300 text-lg rounded-tr-lg py-3 w-1/2">
+                        LỊCH SỬ ĐẶT CHỖ
+                    </div>
+                </div>
+                <div
+                    className="relative"
+                >
+                    <img
+                        className="w-full h-full md:block lg:block hidden rounded-b-2xl object-fit object-center"
+                        style={{height: "500px"}}
+                        src="https://i.imgur.com/3mcNLZX.png"
+                        alt=""
+                    />
+                    <div className="absolute bottom-32 left-24  px-4 py-5 sm:p-0">
+                        <h2 className="text-3xl font-bold text-center text-[#002D74]">Nhập mã đặt chỗ</h2>
+                        <form className="space-y-7 p-12" onSubmit={(e) => e.preventDefault()}>
+                            <div>
+                                <label className="text-gray-600 text-left text-sm font-medium"> Mã đặt chỗ</label>
+                                <input type="text"
+                                       className="w-full border border-gray-300 rounded-lg p-2 mt-1 text-gray-700 text-sm"/>
+                            </div>
+                            <button
+                                className="w-full bg-blue-800 hover:bg-blue-900 text-white font-bold rounded-lg p-3">
+                                Tra cứu
+                            </button>
+                        </form>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    )
 }
 
 export default HomePage;
--- client/src/components/MyBooking/SearchBooking.js
+++ client/src/components/MyBooking/SearchBooking.js
@@ -1,5 +1,152 @@
-function SearchBooking() {
+import React, {useState} from "react";
+import logo from "../../logo.svg";
 
+function SearchBooking() {
+    const [activeTab, setActiveTab] = useState("outbound");
+    const [roundTrip, setRoundTrip] = useState(true);
+    return (
+        <div className="bg-gray-100 p-10">
+            <link rel="preconnect" href="https://fonts.googleapis.com"/>
+            <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin/>
+            <link
+                href="https://fonts.googleapis.com/css2?family=Barlow+Condensed:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Lilita+One&family=Pacifico&family=Pangolin&family=Potta+One&family=Protest+Revolution&display=swap"
+                rel="stylesheet"/>
+            <div className="flex flex-col space-y-4 pb-4">
+                <h2 className="text-5xl font-bold text-center text-[#002D74]">Mã đặt chỗ của bạn là</h2>
+                <p className="text-3xl font-bold text-center">ENF89P</p>
+            </div>
+            <div className="p-10 bg-gray-100 min-h-screen flex flex-col items-center">
+                {roundTrip && (
+                    <div className="flex space-x-4 mb-6">
+                        <button
+                            className={`py-2 px-4 rounded-lg font-semibold ${
+                                activeTab === "outbound"
+                                    ? "bg-blue-500 text-white"
+                                    : "bg-gray-200 text-gray-800"
+                            }`}
+                            onClick={() => setActiveTab("outbound")}
+                        >
+                            Vé chiều đi
+                        </button>
+                        <button
+                            className={`py-2 px-4 rounded-lg font-semibold ${
+                                activeTab === "return"
+                                    ? "bg-blue-500 text-white"
+                                    : "bg-gray-200 text-gray-800"
+                            }`}
+                            onClick={() => setActiveTab("return")}
+                        >
+                            Vé chiều về
+                        </button>
+                    </div>
+                )}
+
+                <div className="w-full">
+                    {activeTab === "outbound" && (
+                        <div>
+                            <TicketCard/>
+                            <TicketCard/>
+                        </div>
+                    )}
+                    {activeTab === "return" && (
+                        <div>
+                            <TicketCard/>
+                        </div>
+                    )}
+                </div>
+            </div>
+        </div>
+    )
+}
+
+function TicketCard() {
+    return (
+        <div className="bg-white border-4 p-2 mx-52 rounded-2xl shadow-lg">
+            <div className="flex flex-row">
+                <div className="w-full flex-grow flex items-center justify-center p-4"
+                     style={{flex: 5}}>
+                    <div className="flex w-full max-w-3xl text-zinc-50 h-64">
+                        <div className="h-full bg-blue-900 flex items-center justify-center rounded-l-3xl">
+                            <img
+                                src={logo}
+                                className="text-white sm:h-11 rotate-[270deg] filter invert"
+                                alt="QAriline Logo"
+                            />
+                        </div>
+                        <div
+                            className="relative h-full flex flex-col items-center border-dashed justify-between border-2 bg-zinc-900 border-zinc-50">
+                            <div className="absolute rounded-full w-8 h-8 bg-white -top-5"></div>
+                            <div className="absolute rounded-full w-8 h-8 bg-white -bottom-5"></div>
+                        </div>
+                        <div
+                            className="h-full py-4 px-10 bg-blue-100 text-black flex-grow rounded-r-3xl flex flex-col">
+                            <div className="text-center text-yellow-600 text-4xl" style={{fontFamily: "Pacifico"}}>
+                                Economy Class
+                            </div>
+                            <div className="flex mt-8 w-full justify-between items-center">
+                                <div className="flex flex-col items-center">
+                                    <span className="text-4xl font-bold">HAN</span>
+                                    <span className="text-zinc-500 text-sm">Hanoi</span>
+                                </div>
+                                <div className="flex flex-col flex-grow items-center px-10">
+                                    <span className="font-bold text-sm">FL1001</span>
+                                    <div className="w-full flex items-center mt-2">
+                                        <div className="w-3 h-3 rounded-full border-2 border-zinc-900"></div>
+                                        <div
+                                            className="flex-grow border-t-2 border-zinc-400 border-dotted h-px"></div>
+                                        <svg
+                                            viewBox="0 0 16 16"
+                                            fill="currentColor"
+                                            height="2em"
+                                            width="2em"
+                                        >
+                                            <path fill="currentColor" d="M8 13.5v-5l-5 5v-11l5 5v-5L13.5 8z"/>
+                                        </svg>
+                                        <div
+                                            className="flex-grow border-t-2 border-zinc-400 border-dotted h-px"></div>
+                                        <div className="w-3 h-3 rounded-full border-2 border-zinc-900"></div>
+                                    </div>
+                                </div>
+                                <div className="flex flex-col items-center">
+                                    <span className="text-4xl font-bold">SGN</span>
+                                    <span className="text-zinc-500 text-sm">Ho Chi Minh City</span>
+                                </div>
+                            </div>
+                            <div className="flex w-full mt-auto justify-between">
+                                <div className="flex flex-col">
+                                    <span className="text-xs text-gray-500">Date</span>
+                                    <span className="font-mono">09/06/2023</span>
+                                </div>
+                                <div className="flex flex-col">
+                                    <span className="text-xs text-gray-500">Departure</span>
+                                    <span className="font-mono">17:45</span>
+                                </div>
+                                <div className="flex flex-col">
+                                    <span className="text-xs text-gray-500">Passenger</span>
+                                    <span className="font-mono">ENDRICK</span>
+                                </div>
+                                <div className="flex flex-col">
+                                    <span className="text-xs text-gray-500">Seat</span>
+                                    <span className="font-mono">A11</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div
+                    className="text-center my-auto h-full flex justify-center items-center"
+                    style={{flex: 1}}
+                >
+                    <button
+                        type="button"
+                        className="items-center py-2.5 px-6 text-sm rounded-lg bg-red-500 text-white cursor-pointer font-semibold text-center shadow-xs transition-all duration-500 hover:bg-red-700"
+                    >
+                        Hủy vé
+                    </button>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default SearchBooking;
