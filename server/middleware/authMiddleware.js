332a3156b30ad3fbf7e344dbb5bde6f3f97dc128
tumo2208 <tuadc1234@gmail.com>
2024-12-05 11:34:47 +0700

    hotfix5: middleware

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -6,7 +6,9 @@ require("dotenv").config();
 const userVerification = async (req, res, next) => {
     const token = req.cookies.token;
     if (!token) {
-        return res.status(400).json({ message: "No token, authorization denied" });
+        // return res.status(400).json({ message: "No token, authorization denied" });
+        req.user = null;
+        return next();
     }
 
     try {
@@ -15,18 +17,24 @@ const userVerification = async (req, res, next) => {
         // Check if the token is blacklisted
         const blacklisted = await redisClient.SISMEMBER("blacklisted_token", token);
         if (blacklisted) {
-            return res.status(400).json({ message: "Token has been blacklisted, please login again" });
+            req.user = null;
+            return next();
+            // return res.status(400).json({ message: "Token has been blacklisted, please login again" });
         }
         const user = await User.findById(decoded.id);
         if (!user) {
-            return res.status(400).json({ message: "User not found" });
+            // return res.status(400).json({ message: "User not found" });
+            req.user = null;
         }
         req.user = user;
-        next();
+        // next();
     } catch (error) {
         console.error(error);
-        return res.status(500).json({ message: "Server error" });
+        // return res.status(500).json({ message: "Server error" });
+        req.user = null;
     }
+
+    return next();
 
 };
 
